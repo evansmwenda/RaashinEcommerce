9661f17dbd1f89a490a78e2eba61f80098483f93
evansmwenda <evansmwenda.em@gmail.com>
2020-10-15 15:57:30 +0300

    product details page done

--- lib/widgets/product_details_widget.dart
+++ lib/widgets/product_details_widget.dart
@@ -9,6 +9,7 @@ class ProductDetailsWidget extends StatefulWidget {
 class _ProductDetailsWidgetState extends State<ProductDetailsWidget> {
   @override
   Widget build(BuildContext context) {
+    var device_width = MediaQuery.of(context).size.width;
     return ListView(
       shrinkWrap: true,
       children: [
@@ -19,20 +20,13 @@ class _ProductDetailsWidgetState extends State<ProductDetailsWidget> {
             color: Colors.blue,
           ),
         ),
-        Container(
-          margin: EdgeInsets.all(10.0),
-          height: 250.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-          ),
-        ),
         Card(
           child: Container(
             padding: EdgeInsets.all(15.0),
-            width: MediaQuery.of(context).size.width,
+            width: device_width,
             decoration: BoxDecoration(
-              // color: Colors.redAccent
-            ),
+                // color: Colors.redAccent
+                ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -108,7 +102,8 @@ class _ProductDetailsWidgetState extends State<ProductDetailsWidget> {
                         ),
                         Text(
                           "(2 ratings)",
-                          style: TextStyle(fontSize: 18.0, color: Colors.indigo),
+                          style:
+                              TextStyle(fontSize: 18.0, color: Colors.indigo),
                         ),
                       ],
                     ),
@@ -134,14 +129,391 @@ class _ProductDetailsWidgetState extends State<ProductDetailsWidget> {
             ),
           ),
         ),
-        ListTile(
-          title: Text("Description"),
-          trailing: Icon(
-            Icons.arrow_forward_ios_rounded,
-            size: 20,
+        SizedBox(
+          height: 45.0,
+          width: device_width,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+            ),
+            child: Text(
+              "DELIVERY AND RETURNS INFO",
+              style: TextStyle(
+                color: Colors.grey[500],
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
+        Card(
+          child: Container(
+            // padding: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                // color: Colors.green,
+                ),
+            width: device_width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.airport_shuttle,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "Description",
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "Normally delivered in Nairobi between Monday 19 Oct and Tuesday 20 Oct,"
+                    "allow up to 6 additional business days for other cities. Please check exact dates in the "
+                    "checkout page.",
+                    style: TextStyle(
+                      fontSize: 15.9,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.youtube_searched_for,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "Return Policy",
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "15 days free return",
+                    style: TextStyle(
+                      fontSize: 15.9,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.assignment_turned_in_outlined,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "Warranty",
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "1 Year Warranty",
+                    style: TextStyle(
+                      fontSize: 15.9,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 45.0,
+          width: device_width,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+            ),
+            child: Text(
+              "PRODUCT DETAILS",
+              style: TextStyle(
+                color: Colors.grey[500],
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                onTap: () {},
+                trailing: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  size: 20,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  "Description",
+                  style: TextStyle(
+                    fontSize: 19.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+                child: Text(
+                  "-Connectivity: Bluetooth, USB, TF Card,Aux In,FM Radio and Hands free",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 2.0),
+                child: Text(
+                  "-Speaker Output : 5W",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 2.0),
+                child: Text(
+                  "-Battery Capacity : 4600mAh",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 2.0, bottom: 10.0),
+                child: Text(
+                  "-Charging time : 4 hours...",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 45.0,
+          width: device_width,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+            ),
+            child: Text(
+              "CUSTOMER FEEDBACK",
+              style: TextStyle(
+                color: Colors.grey[500],
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                onTap: () {},
+                trailing: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  size: 20,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  "Product Rating & Reviews",
+                  style: TextStyle(
+                    fontSize: 19.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: RichText(
+                  text: new TextSpan(
+                    text: '',
+                    children: <TextSpan>[
+                      new TextSpan(
+                        text: "4.4/5",
+                        style: new TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.orange,
+                        ),
+                      ),
+                      new TextSpan(
+                          text: "  (9 ratings)",
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.black,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          width: 170,
+                          child: RatingBar(
+                            initialRating: 3,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            ignoreGestures: true,
+                            itemSize: 19.0,
+                            itemBuilder: (context, _) => Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                          ),
+                        ),
+                        Text("2020-10-14"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "I like it",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Good Sound",
+                      style: TextStyle(color: Colors.black, fontSize: 15.0),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("by Rebecca",style: TextStyle(fontSize: 14.5),),
+                        Text(
+                          "Verified Purchase",
+                          style: TextStyle(
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          width: 170,
+                          child: RatingBar(
+                            initialRating: 3,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            ignoreGestures: true,
+                            itemSize: 19.0,
+                            itemBuilder: (context, _) => Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                          ),
+                        ),
+                        Text("2020-10-14"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Like it",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Very Good Loud Sound",
+                      style: TextStyle(color: Colors.black, fontSize: 15.0),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("by Ebrahim Salim",style: TextStyle(fontSize: 14.5),),
+                        Text(
+                          "Verified Purchase",
+                          style: TextStyle(
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
 
+
+
+            ],
+          ),
+        ),
       ],
     );
   }
