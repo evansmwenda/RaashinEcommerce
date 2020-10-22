d75ecb4114ccae26a764f6dd517cd42cd0cc3e8a
evansmwenda <evansmwenda.em@gmail.com>
2020-10-22 16:38:20 +0300

    added my addresses screen

--- lib/screens/my_addresses.dart
+++ lib/screens/my_addresses.dart
@@ -9,12 +9,71 @@ class MyAddresses extends StatefulWidget {
 class _MyAddressesState extends State<MyAddresses> {
   @override
   Widget build(BuildContext context) {
+    var device_width = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text("My Addresses"),
       ),
-      body: Center(
-        child: Text("This is my addresses page"),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: ListView(
+          // padding: EdgeInsets.all(30.0),
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                  ),
+                  height: 50.0,
+                  width: device_width,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 15.0,
+                  ),
+                  child: Text(
+                    "YOUR ADDRESSES",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Evans Mwenda",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text("Umoja 3 Hospital"),
+                          Text("CBD - UON/Globe/Koja/River Road"),
+                          Text("+254718123123"),
+                        ],
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {},
+                        color: Colors.orange[600],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
